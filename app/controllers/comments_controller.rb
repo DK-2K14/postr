1bc23f42155b3788f35eb4529e1b6ddee3e6847b
kaffin <katherin.siracusa@gmail.com>
2014-11-18 09:55:48 -0500

    adding comments model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to 
+    else
+      render "posts/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user: :current_user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
-  resources :posts, only: [:new, :create, :index, :show]
+  resources :posts, only: [:new, :create, :index, :show] do 
+    resources :comments, only: [:create]
+  end
 
   root "posts#index"
 end
