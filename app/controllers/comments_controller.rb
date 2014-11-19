2670cb15d93b68ab3fd40e7ef2f80d7117d2cf09
kaffin <katherin.siracusa@gmail.com>
2014-11-19 16:42:17 -0500

    adding before_actions for votes and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :require_login
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
@@ -11,6 +13,13 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to comment!"
+      redirect_to :back
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:content).merge(user: current_user)
--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,7 +1,18 @@
 class UpvotesController < ApplicationController
+  before_action :require_login
+
   def create
     post = Post.find(params[:post_id])
     current_user.upvote(post)
     redirect_to :back
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to vote!"
+      redirect_to :back
+    end
   end
 end
