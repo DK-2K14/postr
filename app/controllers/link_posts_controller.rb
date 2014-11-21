5f9a67d487901278515e99a1c23e3f922b09446c
kaffin <katherin.siracusa@gmail.com>
2014-11-21 11:39:28 -0500

    Added ability for user to edit, delete posts
    *Added dependent destroy so that posts are deleted along with text/link_posts
    *Only allow owner of post to edit/delete
    *add edit form, then removed form into partial for both edit and new
    *Fix bug relating to redirect_to @post after commenting

--- app/controllers/link_posts_controller.rb
+++ app/controllers/link_posts_controller.rb
@@ -1,4 +1,6 @@
 class LinkPostsController < ApplicationController
+  before_filter :require_owner, only: [:edit, :update, :destroy]
+
   def new
     @link_post = LinkPost.new
   end
@@ -19,9 +21,35 @@ class LinkPostsController < ApplicationController
     @comment = Comment.new
   end
 
+  def edit
+    @link_post = LinkPost.find(params[:id])
+  end
+
+  def update
+    @link_post = LinkPost.find(params[:id])
+    if @link_post.update(link_post_params)
+      redirect_to link_post_path(@link_post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    link_post = LinkPost.find(params[:id])
+    link_post.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def link_post_params
     params.require(:link_post).permit(:url)
+  end
+
+  def require_owner
+    if current_user != LinkPost.find(params[:id]).post.user
+      redirect_to root_path
+    end
   end
 end
--- app/models/dashboard.rb
+++
@@ -1,7 +0,0 @@
-class Dashboard
-  attr_reader :posts
-
-  def initialize(posts = Post.all)
-    @posts = posts
-  end
-end
